2ae93d5a33fd32dc45c23be13b10116f207414fb
samanimkr <samanimkr@outlook.com>
2017-06-04 20:50:45 +0100

    Major redesign to whole page

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
 
-    $('.repositories').hover(function() {
-        $(this).toggleClass('accent-2');
+    $('#projectTab').hide(function(){});
+
+    $('.links').hover(function() {
+        $(this).toggleClass('lighten-2');
+        $(this).toggleClass('lighten-1');
+    });
+
+    $('#github_btn').click(function(){
+        $('#profileTab').hide(function(){});
+        $('#projectTab').show(function(){});
+    });
+    $('#profile_btn').click(function(){
+      $('#projectTab').hide(function(){});
+      $('#profileTab').show(function(){});
     });
 
 });
