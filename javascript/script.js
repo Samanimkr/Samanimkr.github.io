584d07c2ebdbfed28352f2e0af5e92253048a985
samanimkr <samanimkr@outlook.com>
2017-06-07 13:02:48 +0100

    Started making it responsive

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-var repos_shown = 3, i=0, updated_at, last_update;
+var repos_shown = 3, i=0, updated_at;
 $(document).ready(function() {
 
   $.ajax({
@@ -13,42 +13,28 @@ $(document).ready(function() {
     }
   }).done(function(repos){
     $.each(repos, function(index, repo){
-      $.ajax({
-        url:'https://api.github.com/repos/Samanimkr/'+ repo.name+'/commits',
-        data:{
-          /*
-          client_id: '',
-          client_secret: ''
-          */
-          sort: 'updated',
-          direction: 'desc'
-        }
-      }).done(function(commit){
-        last_update = commit[0].commit.message;
-        updated_at = (repo.updated_at).substring(0,10);
-        $('#github-repos').append(`
-          <div class="col s12 m4" id="repo${i}">
-            <div class="card white darken-1">
-              <div class="card-content">
-                <span class="card-title teal-text text-accent-4">${repo.name}</span>
-                <div class="teal-text text-lighten-4">
-                  <p>Description: ${repo.description}</p>
-                  <p>Language: ${repo.language}</p>
-                  <p>Last Update: ${last_update}</p>
-                  <p>Last Modified: ${updated_at}</p>
-                </div>
-              </div>
-              <div class="card-action">
-                <a class="teal-text text-accent-3" href="#">Check it out!</a>
+      updated_at = (repo.updated_at).substring(0,10);
+      $('#github-repos').append(`
+        <div class="col s12 m4" id="repo${i}">
+          <div class="card white darken-1">
+            <div class="card-content">
+              <span class="card-title teal-text text-accent-4">${repo.name}</span>
+              <div class="teal-text text-lighten-3">
+                <p>Description: <strong>${repo.description}</strong></p>
+                <p>Language: <strong>${repo.language}</strong></p>
+                <p>Last Modified: <strong>${updated_at}</strong></p>
               </div>
             </div>
+            <div class="card-action">
+              <a class="teal-text text-accent-3 center" href="#">GitHub Page</a>
+            </div>
           </div>
-        `);
-        if(i>=3){
-          $(('#repo'+i)).css("display", "none");
-        }
-        i++;
-      });
+        </div>
+      `);
+      if(i>=3){
+        $(('#repo'+i)).hide();
+      }
+      i++;
     });
   });
 
