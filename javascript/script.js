bfc73112bbe434160666757e66b6e9e67c4d9a20
Samanimkr <Samanimkr@Outlook.com>
2018-09-05 21:39:15 +0100

    Nav Background

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,15 @@
-
+//Nav Link To: #Home
+$(".nav_links span:nth-of-type(1)").click(function() {
+    $('html,body').animate({scrollTop: $("#home").offset().top},'slow');
+});
+//Nav Link To: #Portfolio
+$(".nav_links span:nth-of-type(2)").click(function() {
+    $('html,body').animate({scrollTop: $("#portfolio").offset().top},'slow');
+});
+//Nav Link To: #Contact
+$(".nav_links span:nth-of-type(3)").click(function() {
+    $('html,body').animate({scrollTop: $("#contact").offset().top},'slow');
+});
 
 // var repos_shown = 3, i=0, updated_at, repocard;
 // $(document).ready(function() {
