69c2b5cd9e25d54128e03d6b951558dabdef0869
Samanimkr <Samanimkr@Outlook.com>
2020-06-06 02:27:41 +0100

    New design for landing page / home

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,12 @@
-//Nav Link To: #Home
-$(".nav_links a:nth-of-type(1)").click(function() {
-    $('html,body').animate({scrollTop: $("#home").offset().top},'slow');
-});
 //Nav Link To: #Portfolio
-$(".nav_links a:nth-of-type(2)").click(function() {
+$(".nav_links a:nth-of-type(1)").click(function() {
     $('html,body').animate({scrollTop: $("#portfolio").offset().top},'slow');
 });
 //Nav Link To: #Contact
-$(".nav_links a:nth-of-type(3)").click(function() {
+$(".nav_links a:nth-of-type(2)").click(function() {
     $('html,body').animate({scrollTop: $("#contact").offset().top},'slow');
 });
 //button link to: #Contact
-$("#hire").click(function() {
-    $('html,body').animate({scrollTop: $("#contact").offset().top},'slow');
+$("#scrollDown").click(function() {
+    $('html,body').animate({scrollTop: $("#portfolio").offset().top},'slow');
 });
