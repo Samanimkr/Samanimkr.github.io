78867e165b3a6115899c4dcb6e72e511ae235dbf
Samanimkr <Samanimkr@Outlook.com>
2020-06-06 15:28:02 +0100

    Changes favicon colour and adds Trustedhousesitters to list of projects

--- javascript/script.js
+++ javascript/script.js
@@ -14,28 +14,42 @@ $("#scrollDown").click(function() {
 
 const projectsData = [
     {
+        name: "TrustedHousesitters",
+        description: "Find pet sitters offering in-home pet care, or stay at places around the world by house sitting and caring for other's pets.",
+        tags: ["React Native", "iOS", "Android"],
+        imageSource: "img/trustedhousesitters.png",
+        link: "https://apps.apple.com/gb/app/trustedhousesitters/id1292606611",
+        linkText: 'VIEW ON APP STORE &nbsp; <i class="fab fa-app-store fa-lg"></i>',
+    },
+    {
         name: "Twitter Analyser",
         description: "Monitors the Twitter activity on the Rocky Horror Show and generates graphs showing real-time statistics.",
         tags: ["Twitter API", "Google Maps API"],
         imageSource: "img/twitter_analyser.png",
+        link: "https://apps.apple.com/gb/app/trustedhousesitters/id1292606611",
+        linkText: 'VIEW PROJECT ON GITHUB &nbsp; <i class="fas fa-arrow-right"></i>',
     },
     {
         name: "8 Hours",
         description: "This is a global online programming competition, where programmers compete to make the best web application, that fits the randomly generated phrase, in 8 hours.",
         tags: ["Node JS", "Socket.io", "Javascript"],
         imageSource: "img/8_hours.png",
+        link: "https://apps.apple.com/gb/app/trustedhousesitters/id1292606611",
+        linkText: 'VIEW PROJECT ON GITHUB &nbsp; <i class="fas fa-arrow-right"></i>',
     },
     {
         name: "Quirk",
         description: "Habit Tracker which motivates and helps you become more consistent by competing against your friends.",
         tags: ["Chart JS", "MongoDB", "Facebook Login API"],
         imageSource: "img/quirk.png",
+        link: "https://apps.apple.com/gb/app/trustedhousesitters/id1292606611",
+        linkText: 'VIEW PROJECT ON GITHUB &nbsp; <i class="fas fa-arrow-right"></i>',
     },
 ];
 
 const projectsList = document.getElementById("portfolio");
 
-projectsData.forEach(project => {
+projectsData.forEach((project, i) => {
     const wrapper = document.createElement("div");
 
     wrapper.innerHTML = `
@@ -44,10 +58,12 @@ projectsData.forEach(project => {
             <div class="project_info">
             <h2>${project.name}</h2>
             <p>${project.description}</p>
-                ${project.tags.map(tag => `<a>${tag}</a>`).join('')}
+            ${project.tags.map(tag => `<a class="tag">${tag}</a>`).join('')}
+            <a class="linkToGithub" href="${project.link}" target="_blank">${project.linkText}</a>
             </div>
         </div>
-        <div class="separator"></div>
+        ${i < (projectsData.length-1) ? '<div class="separator"></div>' : ''}
+        ${i === (projectsData.length-1) ? '<a id="more_projects" href="https://github.com/Samanimkr?tab=repositories" target="_blank">VIEW ALL PROJECTS &nbsp; <i class="fas fa-external-link-alt"></i></a>' : ''}
     `
-    projectsList.prepend(wrapper);
+    projectsList.appendChild(wrapper);
 })
